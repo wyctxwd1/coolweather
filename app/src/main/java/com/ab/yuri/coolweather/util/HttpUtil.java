8ead66bae1474d98bec190e492deb1d28f074a3f
wyctxwd1 <469104809@qq.com>
2017-01-14 18:18:59 +0800

    遍历省市县

---
+++ app/src/main/java/com/ab/yuri/coolweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.ab.yuri.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Yuri on 2017/1/14.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client =new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
